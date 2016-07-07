70354e29efb937c9fb3831f1e93cf8a5b18828cb
julio1411 <julitto_14@hotmail.com>
2016-07-07 13:04:39 -0500

    Update menuprincipal.py

--- menuprincipal.py
+++ menuprincipal.py
@@ -15,3 +15,34 @@ def opciones():
       print("2.- Retiro /deposito")
       print("3.- Prestamo")
       print("4.- Salir")
+
+#Julio
+def verificar(x):
+  for c in x:
+    if((ord(c)<65 or ord(c)>90) and (ord(c)<97 or ord(c)>122) and (ord(c)!=32)):
+      return False
+  return True
+
+def nnombre():
+  global nombre
+  print("Ingrese su Nombre")
+  nombre=input()
+  while(not verificar(nombre)):
+    print("Verifique Error en Nombre")
+    nombre=input()
+    
+def nedad():
+  global edad
+  while(1):
+    try:
+      print("Ingrese su Edad ")
+      edad=input()
+      edad=int(edad)
+      while(edad<=0 or edad>150):
+        print("Ingrese su Edad Correctamente")
+        edad=input()
+        edad=int(edad)
+      break
+    except ValueError:
+      print("Ingrese Su Edad Correctamente")
+    
